54d5f9e447206c79ff359f7d346c275b12dc3887
Foystor <foystor@gmail.com>
2022-06-20 03:52:28 +0800

    chore: Remove unnecessary 'requires'

--- catpoint-parent/securitymodule/src/main/java/module-info.java
+++ catpoint-parent/securitymodule/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module securitymodule {
     requires com.google.common;
     requires com.google.gson;
     requires java.prefs;
-    requires java.datatransfer;
     requires java.desktop;
     requires miglayout;
     requires imagemodule;
