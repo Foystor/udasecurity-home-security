040657746c71e8a4ca454f404e24e479514eba58
Foystor <foystor@gmail.com>
2022-06-20 03:15:14 +0800

    chore: Create a module descriptor for each package

---
+++ catpoint-parent/securitymodule/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module securitymodule {
+    requires com.google.common;
+    requires com.google.gson;
+    requires java.prefs;
+    requires java.datatransfer;
+    requires java.desktop;
+    requires miglayout;
+    requires imagemodule;
+}
