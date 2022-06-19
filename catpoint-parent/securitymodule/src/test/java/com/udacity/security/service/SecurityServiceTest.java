d8b4e6a1d190437387236d27a44d175b06e1009a
Foystor <foystor@gmail.com>
2022-06-20 02:35:31 +0800

    refactor: Move all components into their proper modules

---
+++ catpoint-parent/securitymodule/src/test/java/com/udacity/security/service/SecurityServiceTest.java
@@ -0,0 +1,21 @@
+package com.udacity.security.service;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class SecurityServiceTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
